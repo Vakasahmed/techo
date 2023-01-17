baf931504a90701138cf9d20db2a2d2a8536571e
tajammul1996 <tpzuffari@gmail.com>
2023-01-17 20:24:07 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
+const countryStateMapping = {
+  "india": ["karnataka", "kerala", "andhra pradesh"],
+  "usa": ["texas", "sca", "tampa"]
+}
+
+const countries = ["india", "usa"]
+
 function App() {
+  const [formValues, setFormValues] = useState({
+    email: "",
+    number: "",
+    name: "",
+    country: "usa"
+  });
+
+  const [entries, setEntries] = useState([])
+
+  const submit = (e) => {
+    e.preventDefault();
+    setEntries([...entries, formValues])
+  }
+
+  const onInputChange =(e) => {
+    // console.log(e.target.name)
+    setFormValues({...formValues, [e.target.name]: e.target.value })
+  }
+
+  const renderEntries = () => {
+    return entries.map(({email, name, number, state, country}) => (
+      <div>
+        {`Email-${email}, Name-${name}, Number-${number}, State-${state}, Country-${country}`}
+      </div>
+    ))
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submit} >
+        <input type="email" name="email" placeholder='Email'  onChange={onInputChange} value={formValues.email}/>
+        <input type="text" name="name"  placeholder='Name' onChange={onInputChange} value={formValues.name}/>
+        <input type="text" name="number" placeholder='Number'  onChange={onInputChange} value={formValues.number}/>
+        <select onChange={onInputChange} name="country" value={formValues.country}> 
+          {countries.map((c) => <option value={c} key={c}>{c.toUpperCase()}</option>)}
+        </select>
+
+        <select onChange={onInputChange} name="state" value={formValues.state}>
+          {countryStateMapping[formValues.country].map(s =><option value={s} key={s}>{s.toUpperCase()}</option>)}
+        </select>
+        <button type='submit'>submit data</button>
+      </form>
+
+
+      {renderEntries()}
     </div>
   );
 }
